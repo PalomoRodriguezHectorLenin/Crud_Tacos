7b0d1d0135c9e696e0aaef275ed608defb2cdaac
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-10-13 12:46:22 -0500

    Cambios3

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ const express = require('express');
 const path = require("path");
 const app = express();
 
+var mysql = require('mysql');
+
+var conexion = mysql.createConnection({
+    host: 'localhost',
+    database: 'tacos',
+    user: 'root',
+    password: 'L3usM4fi3r-'
+});
+
+conexion.connect(function(error){
+    if(error){
+        throw error;
+    }else{
+        console.log('CONEXION EXITOSA');
+    }
+});
+conexion.end();
 app.get("/", (req, res)=>{
 
     //res.send("hello world")
