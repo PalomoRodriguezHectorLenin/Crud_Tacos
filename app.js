0cd0e4f3e98cfa0a178ede4c4e55b508d0794a91
PalomoRodriguezHectorLenin <hpalomor2000@alumno.ipn.mx>
2022-10-13 08:56:45 -0500

    Cambios

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
+const path = require("path");
 const app = express();
 
 app.get("/", (req, res)=>{
 
-    res.send("hello world")
+    //res.send("hello world")
+    res.sendFile(path.join(__dirname + "/public/index.html"));
 
 })
 
 app.listen(3000,()=>{
-    console.log("Server corriendo en el puerto ", 3000)
+    console.log("Server corriendo en el puerto ", 3000);
 });
